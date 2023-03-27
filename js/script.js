2c84898d0ce4f1f201830c2b45cbf79039718169
karoline <shiamosan@gmail.com>
2023-03-27 14:06:38 -0300

    alterações visuais com js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLinks(link){
@@ -10,3 +11,36 @@ function ativarLinks(link){
 }
 
 links.forEach(ativarLinks);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search); //pega os parametros dentro da URL
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto)
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas)
