7a481e6518269c1583a99d2a297a574696cd9819
karoline <shiamosan@gmail.com>
2023-03-27 14:21:08 -0300

    galeria de imagens

--- js/script.js
+++ js/script.js
@@ -43,4 +43,25 @@ function eventosPerguntas(pergunta){
     pergunta.addEventListener('click', ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+    if(media){
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
